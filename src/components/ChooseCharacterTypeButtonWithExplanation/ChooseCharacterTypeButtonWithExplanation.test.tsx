6168da3799fcd864ca9c83d5dbe4fd6a3e3ad846
L1ndseyHerman <lherman21@jcu.edu>
2021-08-11 12:01:14 -0400

    Got a Jest TS callback working!

--- src/components/ChooseCharacterTypeButtonWithExplanation/ChooseCharacterTypeButtonWithExplanation.test.tsx
+++ src/components/ChooseCharacterTypeButtonWithExplanation/ChooseCharacterTypeButtonWithExplanation.test.tsx
@@ -4,7 +4,7 @@ import { act } from "react-dom/test-utils";
 
 import ChooseCharacterTypeButtonWithExplanation from "./ChooseCharacterTypeButtonWithExplanation";
 
-let container: HTMLElement = document.createElement("div");
+const container: HTMLElement = document.createElement("div");
 beforeEach(() => {
   document.body.appendChild(container);
 });
@@ -21,6 +21,21 @@ it("renders Bodyguard button + explanation", () => {
         buttonColor="darkolivegreen"
         buttonText="Bodyguard" 
         explanation="Block +1 Magic Damage when using Shield."
+        callback={(arg0: {screenName: string, playerOneCharacterType: string,
+          computerCharacterType: string}) => arg0}
+        /*callback={(arg0: {screenName: string, playerOneCharacterType: string,
+          computerCharacterType: string}) => void;}*/
+
+        /*callback={(arg0:  {screenName: "Element Selection Screen"; playerOneCharacterType: "Bodyguard";
+        computerCharacterType: "Bodyguard";}) => void}*/
+        /*callback={({screenName: "Element Selection Screen", playerOneCharacterType: "Bodyguard",
+        computerCharacterType: "Bodyguard"})}*/
+
+        /*callback={(arg0: {"screenName": "Element Selection Screen", playerOneCharacterType: "Bodyguard",
+        computerCharacterType: "Bodyguard"}) => void}*/
+
+        /*callback="{screenName: "Element Selection Screen", playerOneCharacterType: buttonText,
+        computerCharacterType: computerCharacterType}""*/
         //callback={"Element Selection Screen", buttonText, computerCharacterType}
         //callback={"Number":1}
         //callback={"String", "String", "String"}
@@ -33,7 +48,9 @@ it("renders Bodyguard button + explanation", () => {
   });
   
   //  The "!" tells TS that you're sure the stuff that comes before it is not null. 
-  expect(container.querySelector("button")!.style.background).toBe("darkolivegreen");
+  //  Now ESLint is saying to use "?" instead, bec it checks it during runtime instead of
+  //    assuming you're right or something.
+  expect(container.querySelector("button")?.style.background).toBe("darkolivegreen");
   expect(container.querySelector("button")!.textContent).toBe("Bodyguard");
   expect(container.querySelector("p")!.textContent).toBe("Block +1 Magic Damage when using Shield.");
 
